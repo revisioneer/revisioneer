7fd539b0764ca7b509e420a7698c25fd0684f0ba
Raphael Randschau <nicolai86@me.com>
2013-11-12 15:36:38 +0100

    persist deployments in postgres

--- revisioneer.go
+++ revisioneer.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"database/sql"
 	_ "github.com/lib/pq"
-
+	"github.com/eaigner/hood"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -14,31 +13,38 @@ import (
 	"time"
 )
 
-type Deploy struct {
-	DeployedAt time.Time
+type Deployments struct {
+	Id hood.Id
 	Sha        string
+	DeployedAt time.Time
 }
 
+var hd *hood.Hood
+
 func ListRevisions(w http.ResponseWriter, req *http.Request) {
-	// var revisions []Deploy = make([]Deploy, 0)
- //  _, err := dbmap.Select(&revisions, "SELECT * FROM revisioneer.deployments ORDER BY deployed_at")
- //  if err != nil {
- //  	log.Fatal(err)
- //  }
-	// b, err := json.Marshal(revisions)
-	// w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	// if err == nil {
-	// 	io.WriteString(w, string(b))
-	// } else {
-	// 	io.WriteString(w, "[]")
-	// }
-	io.WriteString(w, "[]")
+	var revisions []Deployments
+  err := hd.OrderBy("deployed_at").Find(&revisions)
+  if err != nil {
+    panic(err)
+  }
+  b, err := json.Marshal(revisions)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if err == nil {
+		if string(b) == "null" {
+			io.WriteString(w, "[]")
+		} else {
+			io.WriteString(w, string(b))
+		}
+
+	} else {
+		io.WriteString(w, "[]")
+	}
 }
 
 func CreateRevision(w http.ResponseWriter, req *http.Request) {
 	dec := json.NewDecoder(req.Body)
 
-	var deploy Deploy
+	var deploy Deployments
 	err := dec.Decode(&deploy)
 	if err != nil && err != io.EOF {
 		log.Fatal(err)
@@ -46,7 +52,11 @@ func CreateRevision(w http.ResponseWriter, req *http.Request) {
 		deploy.DeployedAt = time.Now()
 	}
 
-	// revisions = append(revisions, deploy)
+	 _, err = hd.Save(&deploy)
+  if err != nil {
+    log.Fatal(err)
+  }
+
 	io.WriteString(w, "")
 }
 
@@ -60,43 +70,11 @@ func init() {
 }
 
 func main() {
-	db, err := sql.Open("postgres", "user=nicolai86 dbname=revisioneer sslmode=disable")
+	var err error
+	hd, err = hood.Open("postgres", "user=nicolai86 dbname=revisioneer sslmode=disable")
 	if err != nil {
 		log.Fatal("failed to connect to postgres", err)
 	}
-	defer db.Close()
-
-	rows, err := db.Query(`SELECT * FROM revisioneer.deployments`)
-  if err != nil {
-    fmt.Println("Failed to run query", err)
-    return
-  }
-  cols, err := rows.Columns()
-  if err != nil {
-    fmt.Println("Failed to get columns", err)
-    return
-  }
-
-  // Result is your slice string.
-  rawResult := make([][]byte, len(cols))
-
-  dest := make([]interface{}, len(cols)) // A temporary interface{} slice
-  for i, _ := range rawResult {
-      dest[i] = &rawResult[i] // Put pointers to each string in the interface slice
-  }
-
-  for rows.Next() {
-  	var id int64
-  	var sha string
-  	var date time.Time
-    err = rows.Scan(&id, &sha, &date)
-    if err != nil {
-        fmt.Println("Failed to scan row", err)
-        return
-    }
-
-    fmt.Printf("%#v%#v%#v\n", id, sha, date)
-  }
 
 	var port string = os.Getenv("PORT")
 	if port == "" {
