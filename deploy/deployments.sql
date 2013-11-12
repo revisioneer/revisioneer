224a3de577a9db4a68b1123764ef737b99aeda7e
Raphael Randschau <nicolai86@me.com>
2013-11-12 15:20:49 +0100

    step 1 - basic db connection

--- deploy/deployments.sql
+++ deploy/deployments.sql
@@ -6,7 +6,7 @@ BEGIN;
 SET client_min_messages = 'warning';
 
 CREATE TABLE revisioneer.deployments (
-  id          SERIAL        PRIMARY KEY,
+  id          SERIAL         PRIMARY KEY,
   sha         TEXT           NOT NULL,
   deployed_at TIMESTAMPTZ    NOT NULL DEFAULT NOW()
 );
--- revisioneer.go
+++ revisioneer.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
+
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -16,16 +19,20 @@ type Deploy struct {
 	Sha        string
 }
 
-var revisions []Deploy = make([]Deploy, 0)
-
 func ListRevisions(w http.ResponseWriter, req *http.Request) {
-	b, err := json.Marshal(revisions)
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	if err == nil {
-		io.WriteString(w, string(b))
-	} else {
-		io.WriteString(w, "[]")
-	}
+	// var revisions []Deploy = make([]Deploy, 0)
+ //  _, err := dbmap.Select(&revisions, "SELECT * FROM revisioneer.deployments ORDER BY deployed_at")
+ //  if err != nil {
+ //  	log.Fatal(err)
+ //  }
+	// b, err := json.Marshal(revisions)
+	// w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	// if err == nil {
+	// 	io.WriteString(w, string(b))
+	// } else {
+	// 	io.WriteString(w, "[]")
+	// }
+	io.WriteString(w, "[]")
 }
 
 func CreateRevision(w http.ResponseWriter, req *http.Request) {
@@ -39,7 +46,7 @@ func CreateRevision(w http.ResponseWriter, req *http.Request) {
 		deploy.DeployedAt = time.Now()
 	}
 
-	revisions = append(revisions, deploy)
+	// revisions = append(revisions, deploy)
 	io.WriteString(w, "")
 }
 
@@ -53,6 +60,44 @@ func init() {
 }
 
 func main() {
+	db, err := sql.Open("postgres", "user=nicolai86 dbname=revisioneer sslmode=disable")
+	if err != nil {
+		log.Fatal("failed to connect to postgres", err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT * FROM revisioneer.deployments`)
+  if err != nil {
+    fmt.Println("Failed to run query", err)
+    return
+  }
+  cols, err := rows.Columns()
+  if err != nil {
+    fmt.Println("Failed to get columns", err)
+    return
+  }
+
+  // Result is your slice string.
+  rawResult := make([][]byte, len(cols))
+
+  dest := make([]interface{}, len(cols)) // A temporary interface{} slice
+  for i, _ := range rawResult {
+      dest[i] = &rawResult[i] // Put pointers to each string in the interface slice
+  }
+
+  for rows.Next() {
+  	var id int64
+  	var sha string
+  	var date time.Time
+    err = rows.Scan(&id, &sha, &date)
+    if err != nil {
+        fmt.Println("Failed to scan row", err)
+        return
+    }
+
+    fmt.Printf("%#v%#v%#v\n", id, sha, date)
+  }
+
 	var port string = os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
